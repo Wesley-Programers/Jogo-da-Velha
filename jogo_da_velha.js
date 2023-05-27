5b1e90128afaa85474d46403b8b343a376f26672
Wesley-Programmers <123242004+Wesley-Programers@users.noreply.github.com>
2023-05-27 10:24:18 -0300

    jogo_da_velha.js

---
+++ jogo_da_velha.js
@@ -0,0 +1,233 @@
+var rodada = document.getElementById('h1-rodada');
+setTimeout(function() {
+    rodada.innerHTML = 'VEZ DE: JOGADOR 1';
+}, 5000);
+
+setTimeout(function() {
+    rodada.innerHTML = 'VEZ DE: JOGADOR 2';
+}, 10000);
+
+
+function quadrado1() {
+    // alert('1')
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad1.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad1.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad1.style.fontSize = '70px';
+    // quad1.innerHTML = 'X';
+    // quad2.style.fontSize = '70px'
+};
+
+// quadrado1();
+
+
+function quadrado2() {
+    // alert('2');
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    // let rodada = document.getElementById('h1-rodada');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad2.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad2.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+
+    // quad2.innerHTML = 'O';
+    quad2.style.fontSize = '70px';
+};
+
+function quadrado3() {
+    
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad3.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad3.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad3.style.fontSize = '70px';
+
+};
+
+function quadrado4() {
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad4.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad4.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad4.style.fontSize = '70px';
+
+};
+
+function quadrado5() {
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad5.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad5.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad5.style.fontSize = '70px';
+
+};
+
+function quadrado6() {
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad6.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad6.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad6.style.fontSize = '70px';
+
+};
+
+function quadrado7() {
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad7.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad7.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad7.style.fontSize = '70px';
+
+};
+
+function quadrado8() {
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad8.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad8.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad8.style.fontSize = '70px';
+
+};
+
+function quadrado9() {
+
+    let quad1 = document.getElementById('content-1');
+    let quad2 = document.getElementById('content-2');
+    let quad3 = document.getElementById('content-3');
+    let quad4 = document.getElementById('content-4');
+    let quad5 = document.getElementById('content-5');
+    let quad6 = document.getElementById('content-6');
+    let quad7 = document.getElementById('content-7');
+    let quad8 = document.getElementById('content-8');
+    let quad9 = document.getElementById('content-9');
+
+    if(rodada.innerHTML === 'VEZ DE: JOGADOR 1') {
+        quad9.innerHTML = 'X';
+        // quad2.innerHTML = 'X';
+    } else if(rodada.innerHTML === 'VEZ DE: JOGADOR 2') {
+        quad9.innerHTML = 'O';
+        // quad2.innerHTML = 'O';
+    };
+    
+    quad9.style.fontSize = '70px';
+
+};
